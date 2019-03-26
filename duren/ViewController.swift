b0ae5ca0df239876b117b226363a1b38b7c87409
Baskoro Nugroho <bas.nugroho@yahoo.com>
2019-03-26 07:57:05 +0700

    first commit

---
+++ duren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  duren
+//
+//  Created by Baskoro Nugroho on 25/03/19.
+//  Copyright © 2019 Baskoro Nugroho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
