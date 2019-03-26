88f874fd19c4160f562666baf404ea70b1d3918f
aditia sugiarto prajitno <adi_dot@yahoo.com>
2019-03-26 17:23:44 +0700

    splashscreen

---
+++ duren/splashScreen.swift
@@ -0,0 +1,30 @@
+//
+//  splashScreen.swift
+//  duren
+//
+//  Created by Aditia sugiarto Prajitno on 26/03/19.
+//  Copyright © 2019 Baskoro Nugroho. All rights reserved.
+//
+
+import UIKit
+
+class splashScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
