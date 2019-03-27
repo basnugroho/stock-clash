53790a31caeed6fea447fa29c75619f84b6609bb
Budi Sugiarto <budisugiarto08@gmail.com>
2019-03-27 15:24:51 +0700

    leaderboard

---
+++ duren/LeaderboardViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (UserDefaults.standard.object(forKey: "user") as? String) != nil
+        {
+            
+        }
+    }
+    
+    public func tab
+}
