dbe60f8c97a40228087771a3b435132a47b5915e
Budi Sugiarto <budisugiarto08@gmail.com>
2019-03-27 15:53:39 +0700

    leaderboard hardcoded

--- duren/LeaderboardViewController.swift
+++ duren/LeaderboardViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var user = UserDefaults.standard.object(forKey: "user") as? String
+    var networth = UserDefaults.standard.object(forKey: "networth") as? String
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,11 +11,22 @@ class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        if (UserDefaults.standard.object(forKey: "user") as? String) != nil
-        {
-            
-        }
+        
     }
     
-    public func tab
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (1)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        
+        if (UserDefaults.standard.object(forKey: "user") as? String) != nil
+        {
+            cell.textLabel?.text = "1.   " + " " + user! + " " + networth!
+        }
+        
+        return(cell)
+    }
 }
