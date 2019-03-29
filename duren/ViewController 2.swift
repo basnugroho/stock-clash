2db4ffdbe0875c6f4a02d2e4c65ec3e31c4ad57c
Leonardo Jeffry <leonardojeffry23@gmail.com>
2019-03-29 19:18:54 +0700

    Sign Out

---
+++ duren/ViewController 2.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  duren
+//
+//  Created by Baskoro Nugroho on 25/03/19.
+//  Copyright © 2019 Baskoro Nugroho. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    /*@IBAction func signoutButton(_ sender: UIButton) {
+    print("LoginScreenViewController")
+        UserDefaults.standard.set(false, forKey: "isUserLoggedIn")
+        UserDefaults.standard.synchronize()
+        
+        let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginScreenViewController
+        
+        let appDel:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        appDel.window?.rootViewController = loginVC
+    }*/
+}
+
