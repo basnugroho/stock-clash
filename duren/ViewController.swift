a1f1d75c9054118bfda6308cdf5fb84570348673
Leonardo Jeffry <leonardojeffry23@gmail.com>
2019-04-01 13:44:28 +0700

    signout

--- duren/ViewController.swift
+++ duren/ViewController.swift
@@ -14,14 +14,15 @@ class ViewController: UIViewController,UIAlertViewDelegate{
         
     }
     @IBAction func signoutButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil);
-        let vc = storyboard.instantiateViewController(withIdentifier: "Loginscreen") ; // MySecondSecreen the storyboard ID
-        self.present(vc, animated: true, completion: nil);
+       
         let alert = UIAlertController(title: "Are you sure to sign out?", message: "That's can make lost your data.", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
             print("Yay! You have signout!")
+            let storyboard = UIStoryboard(name: "Main", bundle: nil);
+            let vc = storyboard.instantiateViewController(withIdentifier: "Loginscreen") ; // MySecondSecreen the storyboard ID
+            self.present(vc, animated: true, completion: nil);
         }))
         self.present(alert, animated: true)
         
