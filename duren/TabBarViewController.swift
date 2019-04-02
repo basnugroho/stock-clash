09785ac833cdf004024b5cc0bae9e5c7cecaaf00
Leonardo Jeffry <leonardojeffry23@gmail.com>
2019-04-02 21:20:00 +0700

    tab bar

---
+++ duren/TabBarViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TabBarViewController.swift
+//  duren
+//
+//  Created by Leonardo Jeffry on 02/04/19.
+//  Copyright © 2019 Baskoro Nugroho. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    class func imageWithColor(color: UIColor, size: CGSize) ->UIImage {
+        let rect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+class CustomTabViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        tabBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tabBar.unselectedItemTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        tabBar.items?[0].image = UIImage(named: "24")
+        tabBar.items?[1].image = UIImage(named: "241")
+        tabBar.items?[2].image = UIImage(named: "242")
+        tabBar.items?[3].image = UIImage(named: "243")
+    }
+}
+
+        // Do any additional setup after loading the view.
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
