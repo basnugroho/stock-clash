1c04ce4922d077991af97e6a4d9e1fecc800bbae
Leonardo Jeffry <leonardojeffry23@gmail.com>
2019-04-03 18:18:28 +0700

    forum

--- duren/PlayingModeViewController.swift
+++ duren/PlayingModeViewController.swift
@@ -59,28 +59,7 @@ class PlayingModeViewController: UIViewController, UIPickerViewDataSource, UIPic
             print("no picker choosen")
         }
     }
-    @IBAction func signoutButton(_ sender: Any) {
-            let alert = UIAlertController(title: "Are you sure to sign out?", message: "That's can make lost your data.", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
-                
-                do {
-                    try Auth.auth().signOut()
-                    self.navigationController?.popToRootViewController(animated: true)
-                } catch {
-                    print("error: there was a problem while sign out")
-                }
-                
-                print("Yay! You have signout!")
-                
-                //old way to sign out
-                let storyboard = UIStoryboard(name: "Main", bundle: nil);
-                let vc = storyboard.instantiateViewController(withIdentifier: "Loginscreen") ; // MySecondSecreen the storyboard ID
-                self.present(vc, animated: true, completion: nil);
-            }))
-            self.present(alert, animated: true)
-        }
+  
     
     /*
     // MARK: - Navigation
